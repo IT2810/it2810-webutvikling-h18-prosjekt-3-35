71282dc4e0510145850ca4d052b4e2a2035fc4eb
Paalar <paaledward@gmail.com>
2018-10-04 19:18:05 +0200

    Beautified the file, added ProgressBar for the pedometer to the homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,58 +1,67 @@
 import React from 'react';
 import {
-  Platform,
-  ScrollView,
-  Image,
-  StyleSheet,
-  View
-  } from 'react-native';
-import { DatePicker } from '../components/DatePicker';
-
+    Platform,
+    ScrollView,
+    Image,
+    StyleSheet,
+    View
+} from 'react-native';
+import {
+    DatePicker
+} from '../components/DatePicker';
+import {
+    PedometerProgressGraph
+} from '../components/PedometerGraph';
+import {
+    AreaChartExample
+} from '../components/ExampleGraph';
 
 const logoSource = '../assets/images/pmm.png';
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-        header: null
-    };
+        static navigationOptions = {
+            header: null
+        };
 
-    render() {
-        return (
-          <View style={styles.container}>
-              <Image
-                source={require(logoSource)}
-                style={styles.logo} />
-              <View style={styles.lineStyle} />
-              <ScrollView style={styles.ScrollView}>
-                  {/*TODO: put exercise list in here*/}
-              </ScrollView>
-          </View>);
-    }
-}
+        render() {
+            return (
+                <View style = {styles.container}>
+                    <Image 
+                        source = {require(logoSource)}
+                        style = {styles.logo}/>
+                    <View style = {styles.lineStyle}/>
+                    <ScrollView style = {styles.ScrollView} >
+                        <PedometerProgressGraph stepsWalked={1000} goal={7000} />
+                        { /*TODO: put exercise list in here*/ } 
+                    </ScrollView>
+                </View>);
+            }
+        }
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'lightgray',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-        paddingHorizontal: 20,
-    },
-    logo: {
-        marginTop: 18,
-        marginBottom: 5,
-        height: 100,
-        width: 100
-    },
-    ScrollView: {
-        backgroundColor: 'lightgray',
-        alignSelf: 'stretch',
-    },
-    lineStyle: {
-        alignSelf: 'stretch',
-        borderWidth: 3,
-        borderColor: 'black',
-        borderBottomColor: 'lightgray',
-        backgroundColor: 'lightgray',
-    }
-});
+        const styles = StyleSheet.create({
+            container: {
+                backgroundColor: 'lightgray',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flex: 1,
+                paddingHorizontal: 20,
+            },
+            logo: {
+                marginTop: 18,
+                marginBottom: 5,
+                height: 100,
+                width: 100
+            },
+            ScrollView: {
+                backgroundColor: 'lightgray',
+                alignSelf: 'stretch',
+            },
+            lineStyle: {
+                alignSelf: 'stretch',
+                borderWidth: 3,
+                borderColor: 'black',
+                borderBottomColor: 'lightgray',
+                backgroundColor: 'lightgray',
+                marginBottom: 10,
+            }
+        });
