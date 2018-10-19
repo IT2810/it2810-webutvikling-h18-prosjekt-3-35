0785644303ba7f0d1d9cb871ae63533e9070a5a9
Paalar <paaledward@gmail.com>
2018-10-19 19:13:47 +0200

    Given functionality previously in homescreen

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
     StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
 import {
     Card,
@@ -8,11 +9,24 @@ import {
 } from 'react-native-paper';
 
 export class ExerciseCard extends Component {
+
+    //Opens the exercise screen and sends down a prop
+    openExerciseGraphScreen = (exercise) => {
+        const {navigation} = this.props;
+        navigation.navigate('ExerciseGraph', {
+            exercise:exercise,
+        });
+    };
+
     render() {
+        const {exercise, index, alertDelete} = this.props;
         return(
-            <Card style={styles.card} theme={{ colors:{text: '#ecf8ff'}}}>
+            <Card
+                onLongPress={() => alertDelete(index, exercise.title)}
+                onPress={() => this.openExerciseGraphScreen(exercise)}
+                style={styles.card} theme={{ colors:{text: '#ecf8ff'}}}>
                 <Card.Content>
-                    <Title>{this.props.title}</Title>
+                    <Title>{exercise.title}</Title>
                 </Card.Content>
             </Card>
         );
