dccb72cb1144fbe122add8768ff3413979d53fd4
Paalar <paaledward@gmail.com>
2018-10-19 16:53:21 +0200

    Updated to the latest versopn of home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,15 @@
-import React, 
-    { Component
-} from 'react';
+import React, { Component } from 'react';
 import {
     ScrollView,
     Image,
     StyleSheet,
     View,
-    Text,
     TouchableOpacity,
     AsyncStorage,
 } from 'react-native';
+import {
+    Button,
+} from 'react-native-paper';
 import { PedometerProgressGraph } from '../components/PedometerGraph';
 import { ExerciseCard } from '../components/ExerciseCard';
 import { PedometerSensor } from '../components/PedometerSensor';
@@ -19,168 +19,199 @@ const addExerciseButton = '../assets/images/plus.png';
 const dailyGoalLocation = 'dailyGoal';
 const exerciseListsLocation = 'exerciseCards';
 
-export default class HomeScreen extends Component {
+export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            pedometerModalVisible: false,
             stepsWalked: 1000,
             stepGoal: 10000,
             exercises: [],
+            exerciseNames: []
         }
     }
+
     static navigationOptions = {
         header: null,
     };
 
-    componentDidMount = () => {
-        this.retrieveData([dailyGoalLocation, exerciseListsLocation]);
-    };
+    // The mount function launches retrieval for the daily goal as well as the exercises created.
+    componentDidMount = () => this.retrieveData();
 
+    //ExerciseNames is used to check if names are unique when creating a new exercise
+    updateUniqueNames = (exerciseList) => {
+        const exerciseNames = []
+        for (const num in exerciseList) {
+            const name = exerciseList[num].title;
+            exerciseNames.push(name);
+        }
+        return exerciseNames;
+    }
+
+    //Takes in a location for the data, as well as the data saved
     saveData = async (location, data) => {
         try {
             await AsyncStorage.setItem(location, JSON.stringify(data));
         } catch (error) {
             console.warn(error);
         }
-    }
+    };
 
-    retrieveData = async (locations) => {
+    //Uses a list of locations to retrieve the data. 
+    retrieveData = async () => {
+        const locations = [dailyGoalLocation, exerciseListsLocation];
         try {
             await AsyncStorage.multiGet(locations)
                 .then((response) =>{
-                    const dailyGoal = JSON.parse(response[0][1]);
-                    const exerciseList = JSON.parse(response[1][1]);
+                    let dailyGoal = JSON.parse(response[0][1]);
+                    let exerciseList = JSON.parse(response[1][1]);
+                    dailyGoal = dailyGoal === null ? 10000 : dailyGoal;
+                    exerciseList = exerciseList === null ? [] : exerciseList;
+                    const exerciseNames = this.updateUniqueNames(exerciseList);
                     this.setState({
                         stepGoal:dailyGoal,
                         exercises:exerciseList,
+                        exerciseNames:exerciseNames,
                     })
                 });
-            
         } catch (error) {
             console.warn(error);
         }
-    }
+    };
 
-    updateSteps = (steps) => {
-        this.setState({stepsWalked:parseInt(steps,10)})
-      }
+    //This function is sent down to PedometerSensor that updates the steps 
+    updateSteps = (steps) => this.setState({stepsWalked:parseInt(steps,10)})
 
+    //This function is sent down to StepGoalScreen to update the step goal, 
+    //and saves the updated goal
     editStepGoal = (goal) => {
         this.setState({
             pedometerModalVisible: !this.state.pedometerModalVisible,
             stepGoal: parseInt(goal, 10),
         });
         this.saveData(dailyGoalLocation, goal);
-    }
+    };
     
-    createExercise = (title, weightType, personalNotes, reps, sets) => {
+    //This function is sent down to CreateExerciseScreen and creates an exercise
+    //based on data retrieved, as well as saving it 
+    createExercise = (title, weightType, personalNotes, reps, sets, goal) => {
         const newExercise = {
             title: title,
             weightType: weightType, 
             personalNotes: personalNotes,
             reps: reps,
             sets: sets,
-        }
+            goal: goal,
+        };
         const exerciseLists = this.state.exercises;
         exerciseLists.push(newExercise);
+        const exerciseNames = this.updateUniqueNames(exerciseLists);
         this.saveData(exerciseListsLocation, exerciseLists);
-        this.setState({exercises:exerciseLists});
-    }
-
-    openExerciseScreen = (exercise) => {
-        this.props.navigation.navigate('ExerciseGraph', {
-            title: exercise.title,
-            weightType: exercise.weightType,
-            personalNotes: exercise.personalNotes,
-            reps: exercise.reps,
-            sets: exercise.sets,
-            goal: exercise.goal,
+        this.setState({
+            exercises:exerciseLists,
+            exerciseNames:exerciseNames,
         });
     }
 
+    //Opens the exercise screen and sends down a prop
+    openExerciseGraphScreen = (exercise) => {
+        this.props.navigation.navigate('ExerciseGraph', {
+            exercise:exercise,
+        });
+    };
+
+    //Opens the StepGoalScreen and sends down props and a function
+    openStepGoalScreen = () => {
+        this.props.navigation.navigate('StepGoal', {
+            currentSteps: this.state.stepsWalked,
+            stepGoal: this.state.stepGoal,
+            acceptChange: this.editStepGoal.bind(this),
+        })
+    }
+
+    //Opens the CreateExereciseScreen 
+    openCreateExerciseScreen = () => {
+        this.props.navigation.navigate('CreateExercise', {
+            createExercise: this.createExercise.bind(this),
+            exerciseNames: this.state.exerciseNames,
+        })
+    }
+
+    //Creates the ExerciseCards that are rendered
     createExerciseCards = () => {
         const exerciseLists = this.state.exercises;
-        const exerciseCards = []
-        //console.log(exerciseLists);
+        const exerciseCards = [];
         for (const num in exerciseLists) {
             exerciseCards.push(
                 <TouchableOpacity 
                     key={num}
-                    onPress={() => this.openExerciseScreen(exerciseLists[num])} >
+                    onPress={() => this.openExerciseGraphScreen(exerciseLists[num])} >
                     <ExerciseCard 
                         title={exerciseLists[num].title}
                     />
                 </TouchableOpacity>);
         }
         return exerciseCards;
-    }
+    };
 
     render() {
         const exerciseCards = this.createExerciseCards();
         return (
             <View style = {styles.container}>
-                <Image 
-                    source = {require(logoSource)}
-                    style = {styles.logo}/>
                 <View style = {styles.lineStyle}/>
                 <ScrollView style = {styles.ScrollView} >
-                    <TouchableOpacity onPress={() => 
-                        this.props.navigation.navigate('StepGoal', {
-                            currentSteps: this.state.stepsWalked,
-                            stepGoal: this.state.stepGoal,
-                            acceptChange: this.editStepGoal.bind(this),
-                        })
-                    }>
+                    <TouchableOpacity onPress={() => this.openStepGoalScreen()}>
+                        <Image
+                            source = {require(logoSource)}
+                            style = {styles.logo}/>
                         <PedometerProgressGraph 
                             stepsWalked={this.state.stepsWalked} 
                             goal={this.state.stepGoal} />
                     </TouchableOpacity>
                     <View>
                         <TouchableOpacity
-                        style={styles.addExerciseView}
-                        onPress={() =>
-                            this.props.navigation.navigate('CreateExercise', {
-                                createExercise: this.createExercise.bind(this)
-                            })}>
-                            <Text>Add exercise</Text>
-                            <Image
-                                //Icon made by wwww.flaticon.com/authors/freepik
-                                source= {require(addExerciseButton)}
-                                style= {styles.addExerciseSymbol}
-                                />
+                            style={styles.addExerciseView}
+                            onPress={() => this.openCreateExerciseScreen()} >
+                            <Button mode={'contained'} dark={true}>Add exercise</Button>
                         </TouchableOpacity>
                     </View>
                     <View style={styles.cardContainer}>
                         {exerciseCards}
                     </View>
                 </ScrollView>
-                <PedometerSensor 
-                    updateSteps={this.updateSteps.bind(this)} 
-                />
+                {/*
+                    <PedometerSensor 
+                        updateSteps={this.updateSteps.bind(this)} 
+                    />
+                */}
             </View>);
-        }
     }
+}
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'lightgray',
+        backgroundColor: '#ecf8ff',
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
+        paddingTop: 10,
         paddingHorizontal: 20,
     },
     logo: {
+        position: 'absolute',
+        zIndex:1000,
+        top: 20,
+        left: 105,
         marginTop: 18,
         marginBottom: 5,
         height: 100,
-        width: 100
+        width: 105
     },
     addExerciseView:  {
         flexDirection: 'row',
         alignItems: 'center',
         alignSelf: 'flex-end',
-        backgroundColor: '#fff',
-        padding: 5,
+        paddingRight: 8,
     },
     addExerciseSymbol : {
         height: 25,
@@ -188,16 +219,7 @@ const styles = StyleSheet.create({
         marginLeft: 5,
     },
     ScrollView: {
-        backgroundColor: 'lightgray',
         alignSelf: 'stretch',
-    },
-    lineStyle: {
-        alignSelf: 'stretch',
-        borderWidth: 3,
-        borderColor: 'black',
-        borderBottomColor: 'lightgray',
-        backgroundColor: 'lightgray',
-        marginBottom: 10,
     },
     cardContainer: {
         flexDirection: 'column',
