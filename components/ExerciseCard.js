e57d2af66dd252a0e3f7253e4915f040341a792f
Paalar <paaledward@gmail.com>
2018-10-19 16:51:43 +0200

    Updated to the latest version of exercise card

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -1,37 +1,28 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
     Text,
-    TouchableOpacity,
 } from 'react-native';
-
+import {
+    Card,
+    Title,
+} from 'react-native-paper';
 
 export class ExerciseCard extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return(
-            <View style={styles.container}>
-                <Text style={styles.title}>{this.props.title}</Text>
-            </View>
+            <Card style={styles.card} theme={{ colors:{text: '#ecf8ff'}}}>
+                <Card.Content>
+                    <Title>{this.props.title}</Title>
+                </Card.Content>
+            </Card>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        backgroundColor: '#fff',
-        alignContent: 'stretch',
-        marginBottom: 5,
-        padding: 10,
-    },
-    title: {
-        fontSize: 18,
+    card: {
+        marginBottom: 2,
     }
 });
