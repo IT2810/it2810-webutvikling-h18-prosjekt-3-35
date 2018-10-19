a670bf2371a6feeda96e5309d8a8bedf77899aff
Paalar <paaledward@gmail.com>
2018-10-19 15:53:27 +0200

    Removed search from to date functionality, added graphs

--- screens/GraphingScreen.js
+++ screens/GraphingScreen.js
@@ -6,45 +6,33 @@ import {
     Text,
     View,
     Button,
-    TouchableOpacity,
-    Alert,
+    ScrollView,
     AsyncStorage,
 } from 'react-native';
-import DateTimePicker from 'react-native-modal-datetime-picker';
-
-const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const twoWeekstime = 12096e5;
+import SessionLineGraph from '../components/SessionLineGraph';
+import SessionCircleGraph from '../components/SessionCircleGraph';
 const sessionLocation = '/sessions';
 
 export default class GraphingScreen extends Component {
     constructor(props) {
         super(props);
-        const today = new Date();
-        today.setHours(23, 59, 0, 0);
-        const fortnightAgo = new Date(today - twoWeekstime);
-        fortnightAgo.setHours(0,0,0,0);
         const {navigation} = this.props;
         const exercise = navigation.getParam('exercise', null);
         this.state = {
-            fromDate: fortnightAgo,
-            toDate: today,
-            clickedDate: null,
-            clickedDateName: '',
-            isDateTimePickerVisible: false,
             exercise: exercise,
-            sessions: []
+            sessions: null,
         }
     }
 
+    //Load the exercise specific sessions
     componentDidMount = () => {
         this.retrieveSessions(this.state.exercise);
     }
 
     saveSessions = async (title, sessions) => {
-        console.log("Saving sessions: ");
-        console.log(sessions)
         try {
+            console.log("Saving session:");
+            console.log(sessions);
             await AsyncStorage.setItem(title + sessionLocation, JSON.stringify(sessions));
         } catch (error) {
             console.warn(error);
@@ -53,16 +41,12 @@ export default class GraphingScreen extends Component {
 
     retrieveSessions = async (exercise) => {
         try {
-    
             await AsyncStorage.getItem(exercise.title + sessionLocation)
                 .then((response) =>{
                     let sessions = JSON.parse(response);
-                    if (sessions === null) {
-                        sessions = [];
-                    }
+                    if (sessions === null) sessions = [];
                     this.setState({
                         sessions:sessions,
-                        exercise:exercise
                     })
                 });
         } catch (error) {
@@ -74,101 +58,89 @@ export default class GraphingScreen extends Component {
         const weekday = weekdays[date.getDay()];
         const month = months[date.getMonth()];
         return weekday + ' ' + date.getDate() + ' ' + month;
-    };
-    setDate = (date, name) => {
-        (name === 'FromDate') ? this.setState({fromDate:date}) : this.setState({toDate:date})
-        this.setState({isDateTimePickerVisible: !this.state.isDateTimePickerVisible,})
-    };
-
-    alertDateMessage = (message) => {
-        Alert.alert(
-            'Unvalid date',
-            message,
-            [{text: 'OK', onPress: () => console.log('OK Pressed')},],
-            { cancelable: false }
-          )
-    };
+    }
 
     openCreateSessionScreen = () => {
-        this.props.navigation.navigate('CreateSession', {
-            weightType:this.state.exercise.weightType,
-            exerciseTitle:this.state.exercise.title,
-            goal:this.state.exercise.goal,
+        const {navigation} = this.props;
+        const {exercise} = this.state;
+        navigation.navigate('CreateSession', {
+            weightType:exercise.weightType,
+            exerciseTitle:exercise.title,
+            goal:exercise.goal,
             createSession:this.createSession.bind(this),
         });
-    };
+    }
 
     createSession = (result, date) => {
+        const {sessions} = this.state;
         const newSession = {
             result: result,
             date: date,
         };
-        const sessionList = this.state.sessions;
-        sessionList.push(newSession);
-        this.saveSessions(this.state.exercise.title, sessionList)
+        const sessionList = sessions == null ? [] : sessions;
+        console.log("sessionlist")
+        console.log(sessionList);
+        if (sessionList.length === 0) {
+            sessionList.push(newSession);
+            this.saveAndSetSessions(sessionList);
+        } else {
+            this.sortSessionInList(sessionList, newSession);
+        }
+    }
+
+    sortSessionInList = (sessionList, newSession) => {
+        const newDate = newSession.date;
+        for (const num in sessionList) {
+            const oldSession = sessionList[num];
+            oldDate = new Date(oldSession.date);
+            if (newDate.getTime() <= oldDate.getTime()) {
+                sessionList.splice(num, 0, newSession);
+                break;
+            } else {
+                sessionList.push(newSession);
+                break;
+            }
+        }
+       this.saveAndSetSessions(sessionList);
+    }
+
+    saveAndSetSessions(sessionList) {
+        this.saveSessions(this.state.exercise.title, sessionList);
         this.setState({sessions:sessionList});
     }
 
-    handleDatePicked = (date) => {
-        if (this.state.clickedDateName === 'FromDate' && this.state.toDate.getTime() < date.getTime()) {
-            const alertMessage = 'The from date cannot be set to be after the to date.';
-            this.alertDateMessage(alertMessage);
-        } else if (this.state.clickedDateName === 'ToDate' && date.getTime() < this.state.toDate.getTime()) {
-            const alertMessage = 'The to date cannot be set to be earlier than the from date.';
-            this.alertDateMessage(alertMessage);
-        } else {
-            (this.state.clickedDateName === 'FromDate') ? this.setState({fromDate:date}) : this.setState({toDate:date});
-        }
-        this.hideDateTimePicker();
-    };
-
-    showHideDatepicker = (name, date) => {
-        this.setState({
-            clickedDateName: name,
-            clickedDate: date,
-            isDateTimePickerVisible: !this.state.isDateTimePickerVisible,
-        });
-    };
-
-    hideDateTimePicker = () => this.setState({isDateTimePickerVisible: false});
-
     render() {
-        if (this.state.exercise === null) {
-            return (false)
+        const {sessions, exercise} = this.state;
+        const weight = exercise.weightType === 'none' ? '' : exercise.weightType;
+        let sessionLineGraph = <View></View>;
+        let sessionCircleGraph = <View></View>;
+        if (sessions != null) {
+            sessionLineGraph = <SessionLineGraph
+                goal={exercise.goal}
+                sessions={sessions} />
+            sessionCircleGraph = <SessionCircleGraph 
+                goal={exercise.goal}
+                sessions={sessions} />
         }
-        const fromDateText = this.getTimeText(this.state.fromDate);
-        const toDateText = this.getTimeText(this.state.toDate);
         return(
-            <View style={styles.container}>
-                <Text style={styles.title}>
-                    {this.state.exercise.title}
-                </Text>
-                <View style={styles.dateButtonRow}>
-                    <TouchableOpacity onPress={()=> this.showHideDatepicker('FromDate', this.state.fromDate)}>
-                        <Text style={styles.dateButton}>From: {fromDateText}</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={()=> this.showHideDatepicker('ToDate', this.state.toDate)}>
-                        <Text style={styles.dateButton}>To: {toDateText}</Text>
-                    </TouchableOpacity>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.title}>{exercise.title}</Text>
+                    <Text>Your goal is {exercise.goal}{exercise.weight}</Text>
+                    {sessionLineGraph}
+                    {sessionCircleGraph}
+                    <Button 
+                        title={'Add new Session'}
+                        onPress={() => this.openCreateSessionScreen()}/>
                 </View>
-                <Button 
-                    title={'Add new Session'}
-                    onPress={() => this.openCreateSessionScreen()}/>
-                <DateTimePicker
-                    date = {this.state.clickedDate}
-                    isVisible = {this.state.isDateTimePickerVisible}
-                    onConfirm = {this.handleDatePicked}
-                    onCancel = {this.hideDateTimePicker}
-                />
-            </View>
+            </ScrollView>
         );
     }
 };
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#ecf8ff',
+
     },
     title: {
         fontSize: 18,
